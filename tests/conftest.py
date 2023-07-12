e7c8dfe72d6559c7cfbb8c57698ca4daa6599146
tropicalmentat <gabtorres1011@gmail.com>
2023-07-12 23:21:52 +0800

    WIP added util function for image to array conversion

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import rasterio as rio
 
 @pytest.fixture
 def pre_goni_cv_opt():
@@ -7,3 +8,25 @@ def pre_goni_cv_opt():
     fp = f'./tests/data/s1b-iw-grd-vh-20200914t095832-20200914t095857-023370-02c637-002.tiff'
 
     return fp
+
+@pytest.fixture
+def MM_LANDSAT8_USGS_20201106_B3():
+
+    fp = f'./tests/data/LO08_L1TP_116050_20201106_20201112_01_T1_B3.tiff'
+
+    return fp
+
+@pytest.fixture
+def MM_LANDSAT8_USGS_20201106_B5():
+
+    fp = f'./tests/data/LO08_L1TP_116050_20201106_20201112_01_T1_B5.tiff'
+
+    return fp
+
+@pytest.fixture
+def landsat_band3(MM_LANDSAT8_USGS_20201106_B3):
+
+    img = None
+    with open(file=MM_LANDSAT8_USGS_20201106_B3,mode='rb') as tif:
+        img = tif.read()
+    return img
--- tests/test_process.py
+++ tests/test_process.py
@@ -1,10 +1,18 @@
-from process_optical import load_img
 import logging
+import process_optical as po
 
 logger = logging.getLogger(__name__)
+logging.getLogger('rasterio').setLevel(logging.CRITICAL)
 
-def test_load_img(pre_goni_cv_opt):
+def test_load_band(MM_LANDSAT8_USGS_20201106_B3):
 
-    img = load_img(pre_goni_cv_opt)    
+    img = po.load_band(MM_LANDSAT8_USGS_20201106_B3)    
 
+    assert False
+
+def test_img_to_array(landsat_band3):
+
+    array, profile = po.image_to_array(img=landsat_band3)
+    logger.debug(array)
+    logger.debug(profile)
     assert False
