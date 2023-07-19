e1d6cad5333e83f5eb4144158d8d367ed530b294
tropicalmentat <gabtorres1011@gmail.com>
2023-07-19 08:42:11 +0800

    WIP changed radiance computation to Data User formula

--- process_optical.py
+++ process_optical.py
@@ -3,27 +3,22 @@ import logging
 import numpy as np
 from math import pi
 from numpy.ma import nomask
+from utils import build_preprocess_metadata
 
 logger = logging.getLogger(__name__)
 
-def dn_to_radiance(array: np.ndarray):
-    # gain and bias are from the 
-    # image .MTL file in the RADIOMETRIC
-    # RESCALING segment
-    # gain = RADIANCE_MULT_<BAND NUMBER>
-    # bias = RADIANCE_ADD_<BAND NUMBER>
-    gain = 0.012059
-    bias = -60.29402
-    
-    # TODO: apply mask to this operation
-    radiance_array = gain*array + bias
+def dn_to_radiance(array: np.ndarray, band: int, metadata: dict):
+    # OLI top-of-atmosphere radiance
+    mult_scaling_factor = metadata[f'RADIANCE_MULT_BAND_{str(band)}']
+    add_scaling_factor = metadata[f'RADIANCE_ADD_BAND_{str(band)}']
+    radiance_array = (mult_scaling_factor * array) + add_scaling_factor
 
     return radiance_array
 
 def radiance_to_reflectance(array: np.ndarray,band: int, metadata: dict):
     # OLI top-of-atmosphere reflectance
-    mult_scaling_factor = metadata[f'RADIANCE_MULT_BAND_{str(band)}']
-    add_scaling_factor = metadata[f'RADIANCE_ADD_BAND_{str(band)}']
+    mult_scaling_factor = metadata[f'REFLECTANCE_MULT_BAND_{str(band)}']
+    add_scaling_factor = metadata[f'REFLECTANCE_ADD_BAND_{str(band)}']
 
     toa_reflectance = (mult_scaling_factor * array) + add_scaling_factor
 
--- tests/test_optical.py
+++ tests/test_optical.py
@@ -5,11 +5,11 @@ import rasterio as rio
 logger = logging.getLogger(__name__)
 logging.getLogger('rasterio').setLevel(logging.CRITICAL)
 
-def test_dn_to_radiance(landsat_band3_masked_array, landsat_band3_profile):
+def test_dn_to_radiance(landsat_band3_masked_array, landsat_metadata, landsat_band3_profile):
     logger.debug(landsat_band3_masked_array)
 
-    radiance = po.dn_to_radiance(array=landsat_band3_masked_array)
-
+    radiance = po.dn_to_radiance(
+        array=landsat_band3_masked_array,band=3,metadata=landsat_metadata)
     landsat_band3_profile['dtype'] = 'float64'
 
     # with rio.open(fp='tests/data/landsat_b3.tiff', mode='w', **landsat_band3_profile) as tif:
@@ -17,15 +17,18 @@ def test_dn_to_radiance(landsat_band3_masked_array, landsat_band3_profile):
     assert False
 
 def test_radiance_to_reflectance(
-        landsat_radiance_b3, landsat_metadata
+        landsat_band3_masked_array, landsat_metadata, landsat_band3_profile
         ):
 
     reflectance = po.radiance_to_reflectance(
-        array=landsat_radiance_b3,band=3,metadata=landsat_metadata
+        array=landsat_band3_masked_array,band=3,metadata=landsat_metadata
     )
 
-    logger.debug(reflectance)
+    logger.debug(reflectance.min())
+    logger.debug(reflectance.max())
 
+    with rio.open(fp='tests/data/landsat_b3_reflectance.tiff', mode='w', **landsat_band3_profile) as tif:
+        tif.write(reflectance, 1)
     assert False
 
 def test_preprocess_landsat():
