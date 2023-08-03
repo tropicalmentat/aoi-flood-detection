02d0a6c1d886073e2105833a5fdaa6b2456d3a0e
tropicalmentat <gabtorres1011@gmail.com>
2023-08-03 22:38:48 +0800

    WIP started dev for extract script for alos2palsar2

--- src/alos2palsar2/extract.py
+++ src/alos2palsar2/extract.py
@@ -2,7 +2,19 @@ from shared.preprocess.alos2palsar2 import (
     calibrate_backscatter,
     speckle_filtering
 )
+import shared.utils as utils
+import logging
+
+logger = logging.getLogger(__name__)
 
 def extract(img_fp: str):
+
+    img = utils.load_image(fpath=img_fp) 
+
+    array, profile = utils.image_to_array(img=img)
+
+    calibrated = calibrate_backscatter(band=array)
+
+    filtered = speckle_filtering()
 
     return
