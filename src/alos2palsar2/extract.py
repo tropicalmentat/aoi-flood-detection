6fde19552ea267feba466f7c84599543bd83ffbc
tropicalmentat <gabtorres1011@gmail.com>
2023-08-17 23:53:54 +0800

    WIP added differencing and thresholding

--- src/alos2palsar2/extract.py
+++ src/alos2palsar2/extract.py
@@ -2,9 +2,7 @@ from shared.preprocess.alos2palsar2 import (
     calibrate_backscatter,
     speckle_filtering
 )
-from shared.utils import get_nodata_mask
 from tempfile import NamedTemporaryFile
-from io import BytesIO
 import numpy as np
 import shared.utils as utils
 import rasterio as rio
@@ -42,10 +40,35 @@ def get_preprocessed(img_fp):
     return preprocessed, profile 
 
 def extract(pre_fp:str, post_fp:str):
-    pre,pre_profile = get_preprocessed(pre_fp)
 
-    post,_ = get_preprocessed(post_fp)
-    logger.debug(len(pre))
-    logger.debug(len(post))
+    with NamedTemporaryFile() as pre_tmp, NamedTemporaryFile() as post_tmp:
+        pre,pre_profile = get_preprocessed(pre_fp)
 
+        post,post_profile = get_preprocessed(post_fp)
+        logger.debug(len(pre))
+        logger.debug(len(post))
+        pre_tmp.write(pre)
+        post_tmp.write(post)
+
+        pre_tmp.seek(0)
+        post_tmp.seek(0)
+
+        pre_mem = np.memmap(
+            filename=pre_tmp.name,dtype='float32',mode='r',shape=(pre_profile['height'],pre_profile['width'])
+        )
+        post_mem = np.memmap(
+            filename=post_tmp.name,dtype='float32',mode='r',shape=(post_profile['height'],post_profile['width'])
+        )
+
+        diff = np.ma.masked_equal(
+            post_mem,value=pre_profile['nodata']) - np.ma.masked_equal(pre_mem,value=pre_profile['nodata'])
+
+        threshold = np.ma.where(diff<-3,1,0)
+
+        with rio.open(
+            fp=f'./tests/data/threshold.tiff',mode='w',
+            width=pre_profile['width'],height=pre_profile['height'],count=1,dtype='int16',
+            transform=pre_profile['transform'],crs=pre_profile['crs'],compress='lzw'
+        ) as tmp:
+            tmp.write(threshold,1)
     return
