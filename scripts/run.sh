af9e2e698c16af7c5ebc649e6ba2d9b188609f65
tropicalmentat <gabtorres1011@gmail.com>
2023-10-04 09:00:46 +0800

    WIP expanded flags and optional args

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,25 +3,32 @@
 # HELP FUNCTON
 ####################
 
-while getopts ":i:f" flag; do
+while getopts ":s:f:i:o:" flag; do
         case $flag in
-                i) # handle image name
-                arg1="$OPTARG"
+                s) # handle image name
+                image="$OPTARG"
                 ;;
                 f) # handle function to run
+                function="$OPTARG"
+                ;;
+                i) # handle input filepath/link
+                inlink="$OPTARG"
+                ;;
+                o) # handle output filepath/link
+                outlink="$OPTARG"
                 ;;
         esac
 done
-echo ${arg1}
+echo ${image}
 
 # TODO: assign command line arguments to environment variables
 # TODO: Create help func to provide instructions on how to run the script
 # TODO: Check if passed var in positional parameter exists
 
 docker run \
-        -v ./src/${arg1}/:/function/src \
+        -v ./src/${image}/:/function/src \
         -v ./shared/:/function/shared \
         -v ./tests/data:/function/src/tests/data \
         -w /function/src \
         --entrypoint pytest \
-        -i msgeo-${arg1} -k test_extract_sentinel1b --log-cli-level=DEBUG
+        -i msgeo-${image} -k test_extract_sentinel1b --log-cli-level=DEBUG
