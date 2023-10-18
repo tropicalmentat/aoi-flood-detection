2fc369dc83ff83d8904bdd59b13d82a47cb7c710
tropicalmentat <gabtorres1011@gmail.com>
2023-10-18 08:48:04 +0800

    WIP added initialized data func

--- src/impact-assessment/overlap.py
+++ src/impact-assessment/overlap.py
@@ -1,2 +1,34 @@
 # for overlap analysis of extracted flood
 # with municipal boundaries
+import json
+import geopandas as gpd
+import logging
+
+logger = logging.getLogger(__name__)
+logging.getLogger('fiona').setLevel(logging.CRITICAL)
+
+def initialize_data(flood_fpath, bounds_fpath):
+
+    flood_df = None
+    flood_crs = None
+    bounds_df = None
+    with open(flood_fpath,mode='r') as flood_src:
+        flood_fc = json.loads(flood_src.read())
+        flood_crs = flood_fc['crs']
+        flood_df = gpd.GeoDataFrame.from_features(flood_fc)
+    
+    bounds_df = gpd.GeoDataFrame.from_file(bounds_fpath)
+    
+    flood_df.set_crs(flood_crs,inplace=True)
+
+    logger.debug(bounds_df.crs)
+    logger.debug(flood_df.crs)
+
+    return flood_df, bounds_df
+
+def overlap_analysis(
+        flood: gpd.GeoDataFrame, bounds: gpd.GeoDataFrame
+):
+    for feature in bounds.iterfeatures():
+        logger.debug(feature)
+    return
--- src/impact-assessment/tests/test_overlap.py
+++ src/impact-assessment/tests/test_overlap.py
@@ -1,5 +1,21 @@
 import pytest
+from .. import overlap as op
+from geopandas import GeoDataFrame
 
-def test_overlap():
+def test_init_data(flood_fp, ph_municity_bounds):
+
+    flood, bounds = op.initialize_data(
+        flood_fpath=flood_fp, bounds_fpath=ph_municity_bounds
+    )
+
+    assert type(flood) is GeoDataFrame and type(bounds) is GeoDataFrame
+
+def test_overlap(data):
+
+    flood_ds, bounds_ds = data
+
+    result = op.overlap_analysis(
+        flood=flood_ds, bounds=bounds_ds
+    )
 
     assert False
