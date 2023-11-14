4dc806e41481ba84274a16521421a364293ffc11
tropicalmentat <gabtorres1011@gmail.com>
2023-11-14 15:09:57 +0800

    WIP tested api with dummy raster

--- src/api/app.py
+++ src/api/app.py
@@ -2,7 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+img_fpath = './tests/data/rasterized.tiff'
+img_bin = None
+
+with open(file=img_fpath, mode='rb') as src:
+    img_bin = src.read()
+
+@app.route("/impact")
 def get_flood_impact():
 
-    return "<p>Hello World!</p>"
+    # return "<p>Hello World!</p>"
+    return img_bin
