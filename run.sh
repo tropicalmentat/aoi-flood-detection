0597736f5c9c5f27724105cc454122391d3d09c1
tropicalmentat <gabtorres1011@gmail.com>
2024-02-28 22:54:35 +0800

    WIP place holders for other sensors

--- run.sh
+++ run.sh
@@ -10,7 +10,19 @@ done
 if [[ $sensor == 'sentinel1b' ]]
 then
     echo "Processing sensor: $sensor";
-    source ./scripts/run_tests.sh
+    source ./scripts/process_sentinel1b.sh
+elif [[ $sensor == 'alos2palsar2' ]]
+then
+    echo "Processing sensor: $sensor";
+
+elif [[ $sensor == 'landsat8' ]]
+then 
+    echo "Processing sensor: $sensor";
+
+elif [[ $sensor == 'sentinel2' ]]
+then
+    echo "Processing sensor: $sensor";
+
 else
-    echo "Argument not understood"
+    echo "Sensor argument not understood"
 fi
