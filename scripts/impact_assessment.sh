2cf25d9292c6d2f309a9df54b2c260f060fbbe66
tropicalmentat <gabtorres1011@gmail.com>
2024-03-29 23:50:21 +0800

    WIP impact assess

--- scripts/impact_assessment.sh
+++ scripts/impact_assessment.sh
@@ -5,12 +5,8 @@ docker run \
         -v ./shared/:/function/src/shared \
         -v ./data/:/function/src/data \
         -w /function/src \
-        --env SENSOR="alos2palsar2" \
-        --env BOUNDS="./data/National_PopAHS_PSA_2020.shp" \
-        --env DB_PATH="./data/source.db" \
-        --env OUTPUT="./data/OUTPUT" \
+        --env SENSOR="$1" \
+        --env BOUNDS="$2" \
+        --env DB_PATH="$3" \
+        --env OUTPUT="$4" \
         -it aoi-impact python main.py
-        # --env SENSOR="$1" \
-        # --env BOUNDS="$2" \
-        # --env DB_PATH="$3" \
-        # --env OUTPUT="$4" \
