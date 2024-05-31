dc3892eb6ca63c911f478d9022c0fe90f2de739c
tropicalmentat <gabtorres1011@gmail.com>
2024-06-01 01:11:56 +0800

    WIP

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,6 +1,7 @@
 
-docker build -t aoi-landsat -f ./src/optical/Dockerfile . &&
-docker build -t aoi-alos2palsar2 -f ./src/alos2palsar2/Dockerfile . &&
-docker build -t aoi-sentinel1b -f ./src/sentinel1b/Dockerfile . &&
-docker build -t aoi-impact -f ./src/impact-assessment/Dockerfile . &&
-docker build -t aoi-api -f ./src/api/Dockerfile .
+docker build -t aoi-db --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/database/Dockerfile .
+docker build -t aoi-landsat --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/optical/Dockerfile . &&
+docker build -t aoi-alos2palsar2 --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/alos2palsar2/Dockerfile . &&
+docker build -t aoi-sentinel1b --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/sentinel1b/Dockerfile . &&
+docker build -t aoi-impact --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/impact-assessment/Dockerfile . &&
+docker build -t aoi-api --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/api/Dockerfile .
