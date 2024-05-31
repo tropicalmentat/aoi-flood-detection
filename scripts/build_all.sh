700769d0c23f94294a370da16f5f40c4a79a1110
tropicalmentat <gabtorres1011@gmail.com>
2024-05-31 18:59:46 +0800

    WIP

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,6 +1,6 @@
 
-docker build -t aoi-landsat -f ./src/landsat/Dockerfile . &&
-docker build -t aoi-alos2palsar2 -f ./src/landsat/Dockerfile . &&
+docker build -t aoi-landsat -f ./src/optical/Dockerfile . &&
+docker build -t aoi-alos2palsar2 -f ./src/alos2palsar2/Dockerfile . &&
 docker build -t aoi-sentinel1b -f ./src/sentinel1b/Dockerfile . &&
-docker build -t aoi-impact -f ./src/impact-assessment . &&
+docker build -t aoi-impact -f ./src/impact-assessment/Dockerfile . &&
 docker build -t aoi-api -f ./src/api/Dockerfile .
