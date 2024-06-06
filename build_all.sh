813f9bc45ea64a238e2d93d5307f5cf8accddd76
tropicalmentat <gabtorres1011@gmail.com>
2024-06-07 01:51:49 +0800

    WIP

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,5 @@
 
-docker build -t aoi-db --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/database/Dockerfile .
+docker build -t aoi-db --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/database/Dockerfile . &&
 docker build -t aoi-landsat --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/optical/Dockerfile . &&
 docker build -t aoi-alos2palsar2 --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/alos2palsar2/Dockerfile . &&
 docker build -t aoi-sentinel1b --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -f ./src/sentinel1b/Dockerfile . &&
